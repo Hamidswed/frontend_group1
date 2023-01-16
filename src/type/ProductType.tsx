a910627434f379c29aa599a44651d13cb5337cc3
Akash <samantray745@gmail.com>
2023-01-16 18:50:13 +0100

    Incomplete NavBar

---
+++ src/type/ProductType.tsx
@@ -0,0 +1,12 @@
+export type ProductType = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+      rate: number;
+      count: number;
+    };
+  };
