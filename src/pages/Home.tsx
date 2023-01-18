2bd89c6a4ff7fcd6283781495b93470f8940b571
HamidReza <98917600+Hamidswed@users.noreply.github.com>
2023-01-18 22:54:31 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 import Hero from "./../components/home/Hero";
 const Home = () => {
   return <Hero />;
 };
 export default Home;
-=======
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
->>>>>>> e3454ed (NavBar and Footer)
