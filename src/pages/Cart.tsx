b5aabc7b994b400ce0c993ed64a5a7d950b0f9fb
HamidReza <98917600+Hamidswed@users.noreply.github.com>
2023-01-18 22:53:55 +0100

    Update Cart.tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import CartList from "../components/cart/CartList"
 
 const Cart=()=>{
   return <CartList/>
 }
 export default Cart
-=======
-import React from 'react'
-
-export default function Cart() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
->>>>>>> e3454ed (NavBar and Footer)
