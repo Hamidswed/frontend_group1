820d3e98bfc701f2e754150702a98fd6842d3367
HamidReza <98917600+Hamidswed@users.noreply.github.com>
2023-01-18 22:56:55 +0100

    Update WishList.tsx

--- src/pages/WishList.tsx
+++ src/pages/WishList.tsx
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import FavoriteList from "../components/wishlist/FavoriteList";
 
 const WishList = () => {
   return <FavoriteList />;
 };
 export default WishList;
-=======
-import React from 'react'
-
-export default function WishList() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
->>>>>>> e3454ed (NavBar and Footer)
