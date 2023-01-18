d607134cc93806a5f141bf979c98fe932824b373
HamidReza <98917600+Hamidswed@users.noreply.github.com>
2023-01-18 22:33:03 +0100

    update CartItem.tsx

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -6,10 +6,6 @@ import {
   TableRow,
   Tooltip,
 } from "@mui/material";
-<<<<<<< HEAD
-=======
-
->>>>>>> e3454ed (NavBar and Footer)
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import { ProductType } from "../../type/ProductType";
 import { useDispatch } from "react-redux";
@@ -35,17 +31,12 @@ const CartItem = ({ cart }: PropType) => {
           <img src={cart.image} alt={cart.title} />
         </TableCell>
         <TableCell align="center">{cart.title.slice(0, 20)}</TableCell>
-<<<<<<< HEAD
         <TableCell align="center">${cart.price * cart.qty}</TableCell>
-=======
-        <TableCell align="center">${cart.price*cart.qty}</TableCell>
->>>>>>> e3454ed (NavBar and Footer)
         <TableCell align="center">{cart.rating.rate}</TableCell>
         <TableCell align="center">
           <Button
             variant="outlined"
             onClick={() => dispatch(actions.removeFromCart(cart))}
-<<<<<<< HEAD
             size="small"
           >
             -
@@ -54,32 +45,16 @@ const CartItem = ({ cart }: PropType) => {
           <Button
             variant="outlined"
             onClick={() => dispatch(actions.addToCart(cart))}
-            size="small"
-=======
-          >
-            -
-          </Button>
-          {cart.qty}
-          <Button
-            variant="outlined"
-            onClick={() => dispatch(actions.addToCart(cart))}
->>>>>>> e3454ed (NavBar and Footer)
           >
             +
           </Button>
         </TableCell>
         <TableCell align="center">
-<<<<<<< HEAD
           <Tooltip title="Remove from cart">
             <IconButton onClick={() => dispatch(actions.removeAll(cart))}>
               <HighlightOffIcon />
             </IconButton>
           </Tooltip>
-=======
-          <IconButton onClick={()=>dispatch(actions.removeAll(cart))}>
-            <HighlightOffIcon />
-          </IconButton>
->>>>>>> e3454ed (NavBar and Footer)
         </TableCell>
       </TableRow>
     </TableBody>
