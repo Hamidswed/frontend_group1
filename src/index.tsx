6f60a2d6440bed1df64a50c68c580c7f1426ed4c
HamidReza <98917600+Hamidswed@users.noreply.github.com>
2023-01-18 22:53:09 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -7,31 +7,15 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store';
->>>>>>> e3454ed (NavBar and Footer)
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-<<<<<<< HEAD
       <BrowserRouter>
         <App />
       </BrowserRouter>
-=======
-<BrowserRouter>
-    <App />
-    </BrowserRouter>
->>>>>>> e3454ed (NavBar and Footer)
     </Provider>
   </React.StrictMode>
 );
